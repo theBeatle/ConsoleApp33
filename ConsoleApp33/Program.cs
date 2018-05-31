3dbc6ff2cead8efb6a8f28a32608294ec64da114
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2018-05-31 17:26:28 +0300

    feature ended

--- ConsoleApp33/Program.cs
+++ ConsoleApp33/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp33
 {
+
+    static class Hello
+    {
+        public static string Hi() => "Hello World!";
+    }
+
     class Program
     {
         static void Main(string[] args)
